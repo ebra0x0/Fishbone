0576ac71c8dff1d23403ed9852d0060369e4c885
ebra0x0 <ebra0x0@gmail.com>
2023-06-19 21:28:10 +0300

    fetch data locally

--- src/screens/Dashboard/OrdersHistory/OrdersHistory.js
+++ src/screens/Dashboard/OrdersHistory/OrdersHistory.js
@@ -3,38 +3,105 @@ import { View, Text, FlatList } from "react-native";
 import styles from "./styles";
 import ScreenHeader from "../../../Components/ScreenHeader/ScreenHeader";
 import Avatar from "../../../Components/Avatar/Avatar";
+import firestore from "@react-native-firebase/firestore";
+import { Skeleton } from "native-base";
+import { useSelector } from "react-redux";
 
 const OrdersHistory = ({ route }) => {
-    const [orders, setOrders] = useState(route.params.orders);
+    const { theme, data } = useSelector((state) => state.user);
+    const [orders, setOrders] = useState([]);
 
     const status = route.params.status;
     const Styles = styles();
 
     useEffect(() => {
-        if (orders?.length) {
-            const sortedOrders = orders;
-            if (status) {
-                sortedOrders.sort(
-                    (a, b) => new Date(b.submitDate.toDate()) - new Date(a.submitDate.toDate())
-                );
-            } else {
-                sortedOrders.sort(
-                    (a, b) => new Date(b.approvalDate.toDate()) - new Date(a.approvalDate.toDate())
-                );
-            }
-            setOrders(sortedOrders);
-        }
+        fetchOrders();
     }, []);
 
+    const fetchOrders = () => {
+        const $User_Ref = firestore().collection("users").doc(data?.id);
+        if (status) {
+            const doneQuery = $User_Ref.collection("orders").where("delievered", "==", true);
+            doneQuery.get().then((orders) => {
+                const doneData = [];
+                orders.forEach(async (order, indx) => {
+                    const { submitDate, source } = order.data();
+
+                    const user = await firestore().collection("users").doc(source).get();
+                    const { photo, Name } = user.data();
+                    doneData.push({
+                        photo,
+                        Name,
+                        submitDate,
+                        source,
+                        key: indx,
+                    });
+
+                    if (doneData.length === orders.size) {
+                        updateOrders(doneData);
+                    }
+                });
+            });
+        } else {
+            const canceledQuery = $User_Ref.collection("orders").where("accepted", "==", false);
+            canceledQuery.get().then((orders) => {
+                const canceledData = [];
+                orders.forEach(async (order, indx) => {
+                    const { approvalDate, source } = order.data();
+
+                    const user = await firestore().collection("users").doc(source).get();
+                    const { photo, Name } = user.data();
+                    canceledData.push({
+                        photo,
+                        Name,
+                        approvalDate,
+                        source,
+                        key: indx,
+                    });
+
+                    if (canceledData.length === orders.size) {
+                        updateOrders(canceledData);
+                    }
+                });
+            });
+        }
+    };
+    const updateOrders = (orders) => {
+        const sortedOrders = orders;
+        if (status) {
+            sortedOrders.sort((a, b) => new Date(b.submitDate.toDate()) - new Date(a.submitDate.toDate()));
+        } else {
+            sortedOrders.sort(
+                (a, b) => new Date(b.approvalDate.toDate()) - new Date(a.approvalDate.toDate())
+            );
+        }
+        setOrders(sortedOrders);
+    };
     const renderItems = ({ item }) => {
         return (
             <View style={Styles.order}>
-                <View style={{ flexDirection: "column", alignItems: "center" }}>
-                    <Avatar profile={{ data: item }} style={Styles.avatar} />
-                    <Text style={Styles.orderName}>{item.Name}</Text>
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar
+                        profile={{ data: { id: item.source, photo: item.photo, Name: item.Name } }}
+                        style={Styles.avatar}
+                    />
+                    <Skeleton.Text
+                        marginTop={3}
+                        lines={1}
+                        startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                        isLoaded={item.Name}
+                    >
+                        <Text style={Styles.orderName}>{item.Name}</Text>
+                    </Skeleton.Text>
                 </View>
 
-                <View style={{ flex: 1, flexDirection: "column", alignItems: "flex-end" }}>
+                <View style={{ flex: 2, flexDirection: "column", alignItems: "flex-end" }}>
                     <View style={{ alignItems: "center" }}>
                         <View
                             style={[
@@ -56,6 +123,41 @@ const OrdersHistory = ({ route }) => {
     };
     const HeaderTitle = route.params.title;
 
+    const renderEmptyComponent = () => {
+        const emptyItems = [1, 2, 3, 4, 5, 6, 7];
+
+        return emptyItems.map((_, indx) => (
+            <View key={indx} style={Styles.order}>
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Skeleton
+                        w={70}
+                        h={70}
+                        rounded={"full"}
+                        startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                    />
+                    <Skeleton.Text
+                        marginTop={3}
+                        lines={1}
+                        startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                    />
+                </View>
+
+                <View style={{ flex: 2, flexDirection: "column", alignItems: "flex-end" }}>
+                    <Skeleton
+                        style={Styles.orderStatus}
+                        startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                    />
+                </View>
+            </View>
+        ));
+    };
+
     return (
         <View style={Styles.container}>
             <ScreenHeader arrow={true} title={HeaderTitle} />
@@ -64,6 +166,7 @@ const OrdersHistory = ({ route }) => {
                 data={orders}
                 renderItem={renderItems}
                 keyExtractor={(item) => item.key}
+                ListEmptyComponent={renderEmptyComponent}
             />
         </View>
     );
--- src/screens/Dashboard/PostsHistory/PostsHistory.js
+++ src/screens/Dashboard/PostsHistory/PostsHistory.js
@@ -3,23 +3,171 @@ import { View, Text, FlatList, Image } from "react-native";
 import styles from "./styles";
 import ScreenHeader from "../../../Components/ScreenHeader/ScreenHeader";
 import Translations from "../../../Languages";
+import firestore from "@react-native-firebase/firestore";
+import { useSelector } from "react-redux";
+import { HStack, Skeleton } from "native-base";
+import { ActivityIndicator } from "react-native";
 
 const PostsHistory = ({ route }) => {
-    const [posts, setPosts] = useState(route.params.posts);
+    const { data, theme } = useSelector((state) => state.user);
+    const [posts, setPosts] = useState([]);
+    const [limit, setLimit] = useState(10);
+    const [lastPost, setLastPost] = useState(0);
+    const [noMore, setNoMore] = useState(false);
+    const [loadingFooter, setLoadingFooter] = useState(false);
+    const [days, setDays] = useState(1);
     const Styles = styles();
+    const { type, title } = route.params;
 
     useEffect(() => {
-        if (posts?.length) {
-            const sortedPosts = posts;
-            sortedPosts.sort((a, b) => new Date(b.date.toDate()) - new Date(a.date.toDate()));
-            setPosts(sortedPosts);
+        switch (type) {
+            case "t":
+                Fetch(1);
+                break;
+            case "w":
+                Fetch(7);
+                setDays(7);
+                break;
+            case "a":
+                Fetch(99);
+                setDays(99);
+                break;
         }
     }, []);
 
     const CONTENT = {
         dashboardStatsticsOrders: Translations().t("dashboardStatsticsOrders"),
     };
+    const $User_Ref = firestore().collection("users").doc(data?.id);
 
+    const Fetch = async (days) => {
+        const daysToSubtract = days;
+        const Now = firestore.Timestamp.now().toDate();
+
+        const desiredDate = new Date(Now);
+        desiredDate.setDate(desiredDate.getDate() - daysToSubtract);
+
+        const Timestamp = firestore.Timestamp.fromDate(desiredDate);
+
+        const querySnapshot = await $User_Ref
+            .collection("posts")
+            .orderBy("date", "desc")
+            .where("date", ">", Timestamp)
+            .limit(limit)
+            .get();
+
+        querySnapshot.size < limit && setNoMore(true);
+
+        const Promises = querySnapshot.docs.map(async (post) => {
+            const { date, postImage, foodType, postDesc, active } = post.data();
+
+            const postCountSnapshot = await $User_Ref
+                .collection("orders")
+                .where("postId", "==", post.id)
+                .get();
+            const postCount = postCountSnapshot.size;
+
+            return {
+                key: post.id,
+                active,
+                date,
+                postImage,
+                foodType,
+                postDesc,
+                ordersCount: postCount,
+            };
+        });
+        const finalPosts = await Promise.all(Promises);
+        if (finalPosts.length == querySnapshot.size) {
+            setLastPost(querySnapshot.docs[querySnapshot.size - 1]);
+            setPosts(finalPosts);
+        }
+    };
+    const fetchMore = async () => {
+        if (loadingFooter) {
+            return;
+        }
+        setLoadingFooter(true);
+        const daysToSubtract = days;
+        const Now = firestore.Timestamp.now().toDate();
+
+        const desiredDate = new Date(Now);
+        desiredDate.setDate(desiredDate.getDate() - daysToSubtract);
+
+        const Timestamp = firestore.Timestamp.fromDate(desiredDate);
+
+        const querySnapshot = await $User_Ref
+            .collection("posts")
+            .where("date", ">", Timestamp)
+            .orderBy("date", "desc")
+            .startAfter(lastPost)
+            .limit(limit)
+            .get();
+
+        if (querySnapshot.size) {
+            const Promises = querySnapshot.docs.map(async (post) => {
+                const { date, postImage, foodType, postDesc, active } = post.data();
+
+                const postCountSnapshot = await $User_Ref
+                    .collection("orders")
+                    .where("postId", "==", post.id)
+                    .get();
+                const postCount = postCountSnapshot.size;
+
+                return {
+                    key: post.id,
+                    active,
+                    date,
+                    postImage,
+                    foodType,
+                    postDesc,
+                    ordersCount: postCount,
+                };
+            });
+            const finalPosts = await Promise.all(Promises);
+            if (finalPosts.length == querySnapshot.size) {
+                setLastPost(querySnapshot.docs[querySnapshot.size - 1]);
+                setPosts((prev) => [...prev, ...finalPosts]);
+                setLoadingFooter(false);
+            }
+        } else {
+            setNoMore(true);
+            setLoadingFooter(false);
+        }
+    };
+    const renderEmptyComponent = () => {
+        const emptyItems = [1, 2, 3, 4, 5, 6, 7];
+
+        return emptyItems.map((_, indx) => (
+            <View key={indx} style={Styles.post}>
+                <Skeleton flex={2} startColor={theme ? "darkBlue.800" : "darkBlue.100"} />
+
+                <View style={{ flex: 1, gap: 10, paddingVertical: 5 }}>
+                    <Skeleton.Text
+                        px={"2"}
+                        w={120}
+                        lines={1}
+                        startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                    />
+                    <Skeleton.Text px={"2"} lines={1} startColor={theme ? "darkBlue.800" : "darkBlue.100"} />
+                    <HStack justifyContent={"space-between"}>
+                        <Skeleton.Text
+                            px={"2"}
+                            w={70}
+                            lines={1}
+                            startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                        />
+                        <Skeleton.Text
+                            px={"2"}
+                            w={170}
+                            lines={1}
+                            startColor={theme ? "darkBlue.800" : "darkBlue.100"}
+                        />
+                    </HStack>
+                </View>
+            </View>
+        ));
+    };
     const renderItems = ({ item }) => {
         return (
             <View style={Styles.post}>
@@ -36,14 +184,27 @@ const PostsHistory = ({ route }) => {
                             {CONTENT.dashboardStatsticsOrders}:
                             <Text style={{ color: "#0dbc79", fontWeight: "bold" }}> {item.ordersCount} </Text>
                         </Text>
-                        <Text style={Styles.postDate}>{item.date.toDate().toDateString()}</Text>
+                        <Text style={Styles.postDate}>{item.date?.toDate().toDateString()}</Text>
                     </View>
                     {item.active && <View style={Styles.actvBuble} />}
                 </View>
             </View>
         );
     };
-    const HeaderTitle = route.params.title;
+    const Loading_Footer = () => {
+        return (
+            <Text
+                style={{
+                    flex: 1,
+                    height: 50,
+                    textAlign: "center",
+                }}
+            >
+                <ActivityIndicator size={25} color="#1785f5" />
+            </Text>
+        );
+    };
+    const HeaderTitle = title;
 
     return (
         <View style={Styles.container}>
@@ -52,7 +213,11 @@ const PostsHistory = ({ route }) => {
                 style={{ paddingHorizontal: 10, paddingTop: 10 }}
                 data={posts}
                 renderItem={renderItems}
+                ListEmptyComponent={renderEmptyComponent}
                 keyExtractor={(item) => item.key}
+                onEndReached={!noMore && fetchMore}
+                onEndReachedThreshold={0.01}
+                ListFooterComponent={loadingFooter ? Loading_Footer : <View style={{ height: 20 }} />}
             />
         </View>
     );
